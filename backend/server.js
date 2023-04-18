0b656832235f25dc087e9d681ff4ed6cc1c6d1da
livivarga <76699785+livivarga@users.noreply.github.com>
2023-04-18 18:44:22 +0200

    create image uploading

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,63 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const port = 9000;
 
-app.get('/', (req, res) => {
+app.use(fileUpload());
+
+app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
-
 app.get("/images", (req, res) => {
   res.sendFile(path.join(`${__dirname}/data/images.json`));
 });
 
-app.use('/public', express.static (`${__dirname}/../frontend/public`));
+app.use("/public", express.static(`${__dirname}/../frontend/public`));
+
+app.post("/upload", (req, res) => {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.image;
+  const newData = {
+    "id": 0,
+    "url": `/${req.body.title}.jpg`,
+    "title": req.body.title,
+    "uploadDate": "2022. 11. 12.",
+    "phName": req.body.phname
+  };
+  uploadPath = `${__dirname}/../frontend/public/img/${req.body.title}.jpg`;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err)
+      return res.status(500).send(err);
+
+    fs.readFile(`${__dirname}/data/images.json`, (err, data) => {
+      if (err)
+        return res.status(500).send(err);
+
+      let imagesData = JSON.parse(data)
+      newData.id = imagesData[imagesData.length - 1].id + 1
+
+      imagesData.push(newData)
+
+      fs.writeFile(`${__dirname}/data/images.json`, JSON.stringify(imagesData, null, 2), (err) => {
+        if (err) res.status(500).send(err);
+
+        console.log('The file has been saved!');
+        res.json(newData);
+      });
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`);
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -14,10 +14,6 @@ const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
 
   // Navigation arrows
   navigation: {
@@ -25,10 +21,6 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
 });
 
 fetch(`/images`)
@@ -41,3 +33,31 @@ fetch(`/images`)
       );
     });
   });
+
+const uploadButton = document.querySelector(".upload-button")
+
+uploadButton.addEventListener("click", () => {
+  console.log("upload")
+
+  const formData = new FormData()
+
+  formData.append("image", document.querySelector(`input[type="file"]`).files[0])
+  formData.append("title", document.querySelector(`input[name="title"]`).value)
+  formData.append("phname", document.querySelector(`input[name="phname"]`).value)
+
+  fetch("/upload", {
+    method: "POST",
+    body: formData
+  })
+    .then(res => {
+      return res.json()
+    })
+
+    .then(resData => {
+      console.log(resData)
+      swiperWrapElement.insertAdjacentHTML(
+        "beforeend",
+        imagesComponent(resData.title, resData.url)
+      );
+    })
+})
